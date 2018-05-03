8e51c28de11e49024298cf5c3b162275844116b1
Pintér Roland László <Roli@Pinter-Roland-MacBook-Pro.local>
2018-05-03 09:37:19 +0200

    Ide fogjuk írni ilyen struktúrában a függvényeket

---
+++ Plotter/function.cpp
@@ -0,0 +1,43 @@
+//C++ includes
+#include <utility>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <string>
+
+using namespace  std;
+
+//Declaration of functions
+vector<double> sin_func(vector<double> x, int n);
+
+
+//Main
+int main()
+{
+    
+    
+    
+    
+    
+    return 0;
+}
+
+
+//Definition of functions
+vector<double> sin_func(vector<double> x, int n)
+{
+    vector<double> y(n);
+    
+    return y;
+}
+
+
+
+/*
+ 
+ git add .
+ got commit -m "message"
+ git push
+ 
+ */
