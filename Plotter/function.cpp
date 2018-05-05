95eeac90b45e69f1644c4cf6c9eb15899b405efe
Pintér Roland László <Roli@Pinter-Roland-MacBook-Pro.local>
2018-05-05 15:53:00 +0200

    added more functions, and created a header file for the signature of the functions used in function.cpp

--- Plotter/function.cpp
+++ Plotter/function.cpp
@@ -7,11 +7,19 @@
 #include <string>
 
 using namespace  std;
+#define domain 1000
 
 //Declaration of functions
-vector<double> sin_func(vector<double> x);
-vector<double> cos_func(vector<double> x);
-vector<double> tg_func(vector<double> x);
+vector<double> x(double starting_point, double ending_point); //domain
+vector<double> sin_func(vector<double> x); //sin
+vector<double> cos_func(vector<double> x); //cos
+vector<double> tg_func(vector<double> x); //tg
+vector<double> sinh_func(vector<double> x); //sin hyperbolic
+vector<double> cosh_func(vector<double> x); //cos hyperbolic
+vector<double> tgh_func(vector<double> x); //tg hyperbolic
+vector<double> exp_func(vector<double x); //exponential
+vector<double> ln_func(vector<double x); //natural logarithm
+
 
 
 //Main
@@ -27,6 +35,17 @@ int main()
 
 
 //Definition of functions
+vector<double> x(double starting_point, double ending_point)
+{
+    vector<double> x(domain);
+    double step = ((ending_point - starting_point) / domain;)
+    for(int i=0; i<domain; ++i)
+    {
+        x[i] = step*(i+1);
+    }
+    
+    return x;
+}
 vector<double> sin_func(vector<double> x, int n)
 {
     vector<double> y(x.size());
@@ -56,6 +75,57 @@ vector<double> tg_func(vector<double> x)
     }
     return y;
 }
+
+vector<double> sinh_func(vector<double> x, int n)
+{
+    vector<double> y(x.size());
+    for(int i=0; i<x.size(); ++i)
+    {
+        y[i] = sinh( x[i] * M_PI / 180);
+    }
+    return y;
+}
+
+vector<double> cosh_func(vector<double> x, int n)
+{
+    vector<double> y(x.size());
+    for(int i=0; i<x.size(); ++i)
+    {
+        y[i] = cosh( x[i] * M_PI / 180);
+    }
+    return y;
+}
+
+vector<double> tgh_func(vector<double> x)
+{
+    vector<double> y(x.size());
+    for(int i=0; i<x.size(); ++i)
+    {
+        y[i] = tanh( x[i] * M_PI / 180);
+    }
+    return y;
+}
+
+vector<double> exp_func(vector<double x)
+{
+    vector<double> y(x.size());
+    for(int i=0; i<x.size(); ++i)
+    {
+        y[i] = exp( x[i] );
+    }
+    return y;
+}
+
+vector<double> ln_func(vector<double x);
+{
+    vector<double> y(x.size());
+    for(int i=0; i<x.size(); ++i)
+    {
+        y[i] = log( x[i] );
+    }
+    return y;
+}
+
 
 
 
---
+++ Plotter/function.h
@@ -0,0 +1,11 @@
+//functions listed and used in function.cpp
+
+vector<double> x(double, double); //domain
+vector<double> sin_func(vector<double>); //sin
+vector<double> cos_func(vector<double>); //cos
+vector<double> tg_func(vector<double>); //tg
+vector<double> sinh_func(vector<double>); //sin hyperbolic
+vector<double> cosh_func(vector<double>); //cos hyperbolic
+vector<double> tgh_func(vector<double>); //tg hyperbolic
+vector<double> exp_func(vector<double>); //exponential
+vector<double> ln_func(vector<double>); //natural logarithm
