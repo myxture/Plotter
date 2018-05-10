4ccddf20f319e9d90523267ecd54f596ccd3256a
Bálint Csurgai-Horváth <cshbalint@gmail.com>
2018-05-10 16:37:34 +0200

    Small modifications and corrections

--- Plotter/function.cpp
+++ Plotter/function.cpp
@@ -1,4 +1,6 @@
 //C++ includes
+#define _USE_MATH_DEFINES
+
 #include <utility>
 #include <iostream>
 #include <vector>
@@ -6,41 +8,14 @@
 #include <fstream>
 #include <string>
 
-using namespace  std;
+using namespace std;
 #define domain 1000
 
-
-
-//Declaration of functions
-vector<double> x(double starting_point, double ending_point); //domain
-vector<double> sin_func(vector<double> x); //sin
-vector<double> cos_func(vector<double> x); //cos
-vector<double> tg_func(vector<double> x); //tg
-vector<double> sinh_func(vector<double> x); //sin hyperbolic
-vector<double> cosh_func(vector<double> x); //cos hyperbolic
-vector<double> tgh_func(vector<double> x); //tg hyperbolic
-vector<double> exp_func(vector<double x); //exponential
-vector<double> ln_func(vector<double x); //natural logarithm
-
-
-
-//Main
-int main()
-{
-    
-    
-    
-    
-    
-    return 0;
-}
-
-
 //Definition of functions
-vector<double> x(double starting_point, double ending_point)
+vector<double> xs(double starting_point, double ending_point)
 {
     vector<double> x(domain);
-    double step = ((ending_point - starting_point) / domain;)
+    double step = ((ending_point - starting_point) / domain);
     for(int i=0; i<domain; ++i)
     {
         x[i] = step*(i+1);
@@ -48,20 +23,20 @@ vector<double> x(double starting_point, double ending_point)
     
     return x;
 }
-vector<double> sin_func(vector<double> x, int n)
+vector<double> sin_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = sin( x[i] * M_PI / 180);
     }
     return y;
 }
 
-vector<double> cos_func(vector<double> x, int n)
+vector<double> cos_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = cos( x[i] * M_PI / 180);
     }
@@ -71,27 +46,27 @@ vector<double> cos_func(vector<double> x, int n)
 vector<double> tg_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = tan( x[i] * M_PI / 180);
     }
     return y;
 }
 
-vector<double> sinh_func(vector<double> x, int n)
+vector<double> sinh_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = sinh( x[i] * M_PI / 180);
     }
     return y;
 }
 
-vector<double> cosh_func(vector<double> x, int n)
+vector<double> cosh_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = cosh( x[i] * M_PI / 180);
     }
@@ -101,27 +76,27 @@ vector<double> cosh_func(vector<double> x, int n)
 vector<double> tgh_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = tanh( x[i] * M_PI / 180);
     }
     return y;
 }
 
-vector<double> exp_func(vector<double x)
+vector<double> exp_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = exp( x[i] );
     }
     return y;
 }
 
-vector<double> ln_func(vector<double x);
+vector<double> ln_func(vector<double> x)
 {
     vector<double> y(x.size());
-    for(int i=0; i<x.size(); ++i)
+    for(int i=0; i<x.size(); ++i)
     {
         y[i] = log( x[i] );
     }
--- Plotter/function.h
+++ Plotter/function.h
@@ -1,11 +1,16 @@
 //functions listed and used in function.cpp
 
-vector<double> x(double, double); //domain
-vector<double> sin_func(vector<double>); //sin
-vector<double> cos_func(vector<double>); //cos
-vector<double> tg_func(vector<double>); //tg
-vector<double> sinh_func(vector<double>); //sin hyperbolic
-vector<double> cosh_func(vector<double>); //cos hyperbolic
-vector<double> tgh_func(vector<double>); //tg hyperbolic
-vector<double> exp_func(vector<double>); //exponential
-vector<double> ln_func(vector<double>); //natural logarithm
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+std::vector<double> xs(double, double); //domain
+std::vector<double> sin_func(std::vector<double>); //sin
+std::vector<double> cos_func(std::vector<double>); //cos
+std::vector<double> tg_func(std::vector<double>); //tg
+std::vector<double> sinh_func(std::vector<double>); //sin hyperbolic
+std::vector<double> cosh_func(std::vector<double>); //cos hyperbolic
+std::vector<double> tgh_func(std::vector<double>); //tg hyperbolic
+std::vector<double> exp_func(std::vector<double>); //exponential
+std::vector<double> ln_func(std::vector<double>); //natural logarithm
+
+#endif FUNCTION_H // FUNCTION_H
