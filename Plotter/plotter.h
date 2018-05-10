55aca0d6056b572a4747e51f23094777bd42d692
Bálint Csurgai-Horváth <cshbalint@gmail.com>
2018-05-10 16:39:22 +0200

    Commiting this again...

--- Plotter/plotter.h
+++ Plotter/plotter.h
@@ -1,4 +1,16 @@
 #ifndef PLOTTER_H
 #define PLOTTER_H
 
+void plot(double xMin, double xMax, std::string funcName, QCustomPlot *sheet);
+
+typedef std::vector<double> (*f)(std::vector<double>);
+std::map<std::string, f> plot_functions {{"sin",  &sin_func },
+                                    {"cos",  &cos_func },
+                                    {"tan",  &tg_func  },
+                                    {"sinh", &sinh_func},
+                                    {"cosh", &cosh_func},
+                                    {"tanh", &tgh_func },
+                                    {"exp",  &exp_func },
+                                    {"ln",   &ln_func  }};
+
 #endif // PLOTTER_H
